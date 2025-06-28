39e6dc452f37e67855253994bb290ebc1fbc0687
Rokenor <a.kovrizhnykh@gmail.com>
2025-06-28 20:22:02 +0300

    fixes for task01, task02

--- task02/main.py
+++ task02/main.py
@@ -21,4 +21,4 @@ def sum_profit(text, func):
 if __name__ == "__main__":
     text = "Загальний дохід працівника складається з декількох частин: 1000.01 як основний дохід, доповнений додатковими надходженнями 27.45 і 324.00 доларів."
     total_income = sum_profit(text, generator_numbers)
-    print(f"Загальний дохід: {total_income}")
+    print(f"Загальний дохід: {total_income}") # Виведе 1351.46
